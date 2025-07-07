61d49d9a5daa16f59aa71a66981472808efc632d
AssafFink <assaf.fink@gmail.com>
2025-07-07 21:30:42 +0300

    localstorage

---
+++ 4-mini-project/index.js
@@ -0,0 +1,88 @@
+// import { CONSTANTS } from "./constants.js";
+
+orderListBackground();
+
+document.querySelector("#add-item").addEventListener("click", addItem);
+
+const trashItems = document.querySelectorAll("i");
+
+for (const trashItem of trashItems) {
+  trashItem.addEventListener("click", removeItem);
+}
+
+function addItem() {
+  const list = document.querySelector("#list");
+  const li = document.createElement("li");
+  const i = document.createElement("i");
+  li.className = "list-group-item";
+  i.className = "bi bi-trash float-end";
+
+  const inputValue = document.querySelector("#item-name").value;
+
+  if (!inputValue) {
+    alert("Please type something...");
+    return;
+  }
+
+  li.append(inputValue, i);
+  list.append(li);
+
+  i.addEventListener("click", removeItem);
+
+  orderListBackground();
+
+  document.querySelector("#item-name").value = "";
+
+  addItemToLocalStorage(li);
+}
+
+function removeItem(event) {
+  if (!confirm("Are you sure?")) {
+    return;
+  }
+  const li = event.target.parentElement;
+  const list = li.parentElement;
+
+  list.removeChild(li);
+
+  removeItemFromLocalStorage(li);
+
+  orderListBackground();
+}
+
+function orderListBackground() {
+  const selectorOddsChild = document.querySelectorAll("li:nth-child(odd)");
+  const selectorEvensChild = document.querySelectorAll("li:nth-child(even)");
+
+  for (const oddElem of selectorOddsChild) {
+    oddElem.classList.add("list-group-item-success");
+  }
+
+  for (const evenElem of selectorEvensChild) {
+    evenElem.classList.remove("list-group-item-success");
+  }
+}
+
+function addItemToLocalStorage(element) {
+  const oldCounter = parseInt(localStorage.getItem("counter"));
+  const counter = oldCounter ? oldCounter + 1 : 1;
+  element.setAttribute("counter", counter);
+  localStorage.setItem("counter", counter);
+
+  let storedList = JSON.parse(localStorage.getItem("list-items"));
+  storedList = storedList ? storedList : {};
+  storedList[counter] = element.outerHTML;
+  localStorage.setItem("list-items", JSON.stringify(storedList));
+}
+
+function removeItemFromLocalStorage(element) {
+  let storedList = JSON.parse(localStorage.getItem("list-items"));
+
+  if (!storedList) {
+    return;
+  }
+
+  const counter = element.getAttribute("counter");
+  delete storedList[counter];
+  localStorage.setItem("list-items", JSON.stringify(storedList));
+}

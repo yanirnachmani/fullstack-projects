febd902c0e62a28ba9c52eb29e640e2b11c76914
AssafFink <assaf.fink@gmail.com>
2025-07-28 21:31:20 +0300

    syncasync

--- 7-execution-context/main.js
+++ 7-execution-context/main.js
@@ -1,16 +1,56 @@
-var x = 1
-function foo() {
-    var y = 2
-    function bar() {
-        var z = 3
-        function baz() {
-            console.log(x);
-            console.log(y);
-            console.log(z);
+// let x = 1
+// function foo() {
+//     {
+//         var y = 2
+//         var x = 5
+//     }
+//     console.log('foo', x);
+//     console.log('foo', y);
+//     var p = 90
+//     // bar()
+// }
+// {
+//     var p = 47
+// }
+// console.log(p);
 
-        }
-        baz()
+// foo()
+// console.log('win', x);
+// bar()
+// function bar() {
+//     var z = 3
+//     function baz() {
+//         console.log('baz', x);
+//         console.log(y);
+//         console.log(z);
+
+//     }
+//     baz()
+// }
+
+
+let x = 1
+
+function foo(y) {
+    let t = 19
+    function boo(z) {
+        return x + y + z + t
     }
-    bar()
+    return boo
 }
-foo()
+
+let f = foo(2)
+
+//f =  function (z) {
+//     return x + y + z + t
+// }
+
+
+
+console.log(f(4));
+//x = 1, z = 4 
+
+console.dir(f);
+
+
+

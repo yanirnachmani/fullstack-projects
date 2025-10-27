aa386317d747fd58a878021d1300e8adbe39ecbd
Yossi Arye <aryeyossi@gmail.com>
2025-10-27 21:29:13 +0200

    ts advanced

---
+++ 15-typescript/type-definition/index.ts
@@ -0,0 +1,4 @@
+import _ from 'lodash'
+
+const random = _.random(23, 78)
+console.log(random);
---
+++ 15-typescript/type-definition/types/index.d.ts
@@ -0,0 +1,3 @@
+// declare module 'lodash' {
+//     export function random(a: string, b: number): number
+// }
